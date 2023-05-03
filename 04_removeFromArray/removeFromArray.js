4f9af9d9ad2cc7322dbaffae8f897926f7506278
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-05-03 14:37:29 +0600

    Test on removing element(s) from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // const resultArr = [];
+    // for (let i = 0; i < array.length; i++) {       //method 1 using for raw loops
+    //     let addElement = true;
+    //     for (let j = 0; j < args.length; j++) {
+    //         if(array[i] === args[j]) {
+    //             addElement = false;
+    //             break;
+    //         }
+    //     }
+    //     if(addElement) {
+    //         resultArr.push(array[i]);
+    //     }
+    // }
+    // return resultArr;
 
+
+    // array.forEach(element => {               //method 2 using forEach
+    //     if(!args.includes(element)) {
+    //         resultArr.push(element);
+    //     }
+    // });
+
+    // return resultArr;
+
+
+    return array.filter(element => !args.includes(element)); //method 3 using filter
 };
 
 // Do not edit below this line
