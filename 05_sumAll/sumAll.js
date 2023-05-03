dfe2774d2ac71d0a98ffd96ef428e73b8afe2f15
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-05-04 04:43:44 +0600

    Practice 05_sumAll TDD excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 <0 || typeof num1!=="number" || typeof num2!=="number") {
+        return `ERROR`;
+    } else {
+        let start, end, result = 0;
+
+        if(num1<num2) {
+            start = num1;
+            end = num2;
+        } else {
+            start = num2;
+            end = num1;
+        }
+    
+        for (let i = start; i <= end; i++) {
+            result += i;
+        }
+    
+        return result;
+    }
 };
 
 // Do not edit below this line
