e4a53ff7ffa07367a439dda5ed57d63cb93503cc
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-05-23 20:45:08 +0600

    Practice JS Array functions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    // const thisYear = new Date().getFullYear();
+    // let maxAge = 0, age = 0, obj;
+
+    // for (const person of people) {
+    //     if(('yearOfDeath' in person)) {
+    //         age = person.yearOfDeath - person.yearOfBirth;
+    //     } else {
+    //         age = thisYear - person.yearOfBirth;
+    //     }
+        
+    //     if(maxAge < age) {
+    //         maxAge = age;
+    //         obj = person;
+    //     }
+    // }
+    // return obj;
+
+   return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+   });
 
 };
+
+function getAge(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
