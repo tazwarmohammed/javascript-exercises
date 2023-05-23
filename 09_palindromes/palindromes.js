3a3d27946cd1aa56f7dfac342ceabcaa5d8fcebb
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-05-23 16:33:11 +0600

    Practice Array manipulation JS

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newString = str.replace(/[^a-zA-Z]/g, "");
+    for (let i = 0, j = newString.length-1; i<=j; i++, j--) {
+        if(newString[i].toLowerCase() !== newString[j].toLowerCase()) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
